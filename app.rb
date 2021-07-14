fa3555ebfdfee26670617b9962586d92024985b1
Pav Rao <pav0107@gmail.com>
2021-07-14 17:18:43 +0100

    correcting typo

--- app.rb
+++ app.rb
@@ -17,7 +17,6 @@ class Battle < Sinatra::Base
     session['player_1_name'] = params[:player_1_name]
     session['player_2_name'] = params[:player_2_name]
     redirect '/play'
-    erb(:play)
   end 
 
   get '/play' do
